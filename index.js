ac04e00bcdd055006416a22eb4d9932371ae223f
tsoupionis <thomassoup@yahoo.com>
2023-02-16 00:44:34 -0500

    Drum Kit Website Uppdate 2/16

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) { 
     document.querySelectorAll("button")[i].addEventListener("click", function (){
-        alert("Click");
+        this.style.color = "white";
     
         // What to do when click is detected
     });
 }
 
-// 
+// var audio = new Audio('sounds/tom-1.mp3');
+// audio.play();
